dff3334cb49d630ae86f72975a9a3df7f2e2bb5e
Do Tran <vivaladaman@gmail.com>
2024-05-14 14:47:58 +0700

    fix second interval -> milisecond interval, add random and jitter

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use enigo::{Direction, Enigo, Key, Keyboard, Settings};
+use enigo::{Direction, Enigo, Key, Keyboard, Mouse, Settings};
+use rand::prelude::*;
 use std::time::Duration;
 use tokio::{self, time::sleep};
 
 #[tokio::main]
 async fn main() {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    let mut rng = thread_rng();
+
     loop {
-        enigo.key(Key::Meta, Direction::Click).unwrap();
-        sleep(Duration::from_secs(1500)).await;
+        if rand::random() {
+            enigo.key(Key::Meta, Direction::Click).unwrap();
+        }
+
+        if rand::random() {
+            enigo.move_mouse(0, 0, enigo::Coordinate::Rel).unwrap();
+        }
+
+        if rand::random() {
+            enigo.scroll(1, enigo::Axis::Vertical).unwrap();
+        }
+
+        let jitter = rng.gen_range(1000..=1500);
+        sleep(Duration::from_millis(jitter)).await;
     }
 }
